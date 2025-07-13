fc423f09e0263f7a5b4f757b16163e5a9de354eb
Karthiki72 <karthiki7272@gmail.com>
2025-07-13 22:50:45 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,109 @@
+// let num = 100;
+
+// if(num%10==0){
+//     console.log("good");
+// }else{
+//     console.log("bad");
+// }
+
+
+
+
+// let name = prompt("enter your name: ");
+// let age = prompt("enter your age: ");
+
+// console.log(alert(`${name} is ${age} years old`));
+
+
+
+
+// let num1=2;
+// switch(num1) {
+//     case 1:
+//         console.log("January,February,March");
+//         break;
+//     case 2:
+//         console.log("April,May,June");
+//         break;
+//     case 3:
+//         console.log("July,August,September");
+//         break
+//     case 4:
+//         console.log("October,November,December");
+//         break;
+//     default:
+//         console.log("enter correct number to view")
+// }
+
+
+
+// let str="apple"
+
+// if((str.length)>=5 && (str[0]==="a" || str[0]==="A")){
+//     console.log("golden word")
+// }else{
+//     console.log("not a golden word")
+// }
+
+
+// let n1=10;
+// let n2=11;
+// let n3=9;
+
+// if(n1>n2 && n1>n3){
+//     console.log("n1 is greater than n2 and n3")
+// }
+// if(n2>n1 && n2>n3){
+//     console.log("n2 is greater than n1 and n3")
+// }
+// if(n3>n2 && n3>n1){
+//     console.log("n3 is greater than n2 and n1")
+// }
+
+
+// let a=42;
+// let b=1002;
+
+// if((a%10)==(b%10)){
+//     console.log(`both numbers have the same last digit that is ${a%10}`);
+// }else{
+//     console.log("both does not contains same last digit")
+// }
+
+
+// let arr = [1,2,3,4,3,2,1];
+
+// for ( let i=0 ; i<=arr.length; i++){
+//     if(arr[i]==2){
+//         arr.splice(i,1);
+//     }
+// }
+
+
+// let a="2345";
+// let x=0;
+// for ( count of a){
+//     console.log(count)
+// }
+// console.log(count);
+
+
+// function dice() {
+//     console.log(Math.floor(Math.random() * 6 )+1);
+// }
+
+// dice();
+
+
+// function avg3(n,b,c){
+//     let avg = ((n+b+c)/3);
+//     console.log(avg);
+// }
+
+// let table = prompt("enter a number");
+// function tab(n) {
+//     for(let i=1; i<=10; i++){
+//         console.log(n*i);
+//     }
+// }
+// tab(table);
